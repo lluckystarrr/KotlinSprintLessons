086854a09824373c61d5110d3068ffbab1191c83
Alena <luckystar@yandex-team.ru>
2025-11-25 21:42:32 +0300

    Task-1-2 workers

---
+++ src/main/kotlin/lesson_1/Task_2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main () {
+    val numberOfOders: Int = 75
+    val thanksMessage: String = "Спасибо за заказ!"
+
+    println(numberOfOders)
+    println(thanksMessage)
+
+//    val numbersOfWorkers: Int = 2000
+//    println(numbersOfWorkers)
+
+    val numbersOfWorkers: Int = 1999
+    println(numbersOfWorkers)
+}
