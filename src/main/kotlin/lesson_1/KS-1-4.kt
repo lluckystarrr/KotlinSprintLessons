b56423df74217bca6ef766b208acd86c8311deec
Alena <luckystar@yandex-team.ru>
2025-11-26 17:44:49 +0300

    KS-1-5 show values

---
+++ src/main/kotlin/lesson_1/KS-1-4.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main ()
+{
+    val length: String = "40_868_600_000"
+    val age: Short = 27
+    val partOfDay: Double = 0.075
+    val seconds: Int = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    print(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+}
