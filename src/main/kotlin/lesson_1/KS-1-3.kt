b17924f4acb3b08da340f529b03d852919017311
Alena <luckystar@yandex-team.ru>
2025-11-26 14:34:45 +0300

    KS-1-3 change space info

---
+++ src/main/kotlin/lesson_1/KS-1-3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    var hourLaunch = "09"
+    var minuteLaunch = "07"
+
+//    println(year)
+//    println(hourLaunch)
+//    println(minuteLaunch)
+
+    var hourLanding = "10:"
+    var minuteLanding = "50"
+
+    println(year)
+    print(hourLanding)
+    print(minuteLanding)
+}
