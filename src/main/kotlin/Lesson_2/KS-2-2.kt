a912add6b6d2a3436a57ecd5d94db2b40d28ae70
Alena <luckystar@yandex-team.ru>
2025-11-27 17:43:54 +0300

    KS-2-2 count salary

---
+++ src/main/kotlin/Lesson_2/KS-2-2.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_2
+
+fun main() {
+
+    val workers = 50
+    var workersSalary = 30000
+    val intern = 30
+    val internsSalary = 20000
+
+println(workersSalary * workers)
+    println((workersSalary * workers) + (internsSalary * intern))
+    println((workersSalary + internsSalary)/2)
+
+
+}
