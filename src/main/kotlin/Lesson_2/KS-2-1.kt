042c16222e0ef2162fc2f11bffe0c017973b50f4
Alena <luckystar@yandex-team.ru>
2025-11-27 16:44:10 +0300

    KS-2-1 count average value

---
+++ src/main/kotlin/Lesson_2/KS-2-1.kt
@@ -0,0 +1,12 @@
+package org.example.Lesson_2
+
+fun main() {
+    val firstStudent: Float = 3f
+    val secondStudent: Float = 4f
+    val thirdStudent: Float = 3f
+    val forthStudent: Float = 5f
+
+    val sum = (firstStudent + secondStudent + thirdStudent + forthStudent)/4
+
+    println(sum)
+}
