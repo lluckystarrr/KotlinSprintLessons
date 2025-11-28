63600f3b4f8d7fad9f4908458f77f6d5777a057c
Alena <luckystar@yandex-team.ru>
2025-11-28 15:05:22 +0300

    KS-2-4 count buff

---
+++ src/main/kotlin/lesson_2/KS-2-4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_2
+
+fun main() {
+    val cristal = 7
+    val iron = 11
+    val buffPercsent = 20
+
+    val cristalBuff  = cristal * buffPercsent / 100
+    val ironBuff = iron * buffPercsent / 100
+
+    println(ironBuff)
+    println(cristalBuff)
+}
