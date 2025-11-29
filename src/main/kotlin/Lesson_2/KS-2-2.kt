62fa0edd60c345e7e82c210d38b871f866f736ff
Alena <luckystar@yandex-team.ru>
2025-11-29 16:01:48 +0300

    KS-2-2 recount salaries

--- src/main/kotlin/Lesson_2/KS-2-2.kt
+++ src/main/kotlin/Lesson_2/KS-2-2.kt
@@ -6,10 +6,16 @@ fun main() {
     var workersSalary = 30000
     val intern = 30
     val internsSalary = 20000
+    val allWorkersSalaries = workersSalary * workers
 
-println(workersSalary * workers)
-    println((workersSalary * workers) + (internsSalary * intern))
-    println((workersSalary + internsSalary)/2)
+    println(allWorkersSalaries)
 
+    val allInternsSalaries = internsSalary * intern
 
+    println(allWorkersSalaries + allInternsSalaries)
+
+    val allPeople = intern + workers
+    val allSalaries = allWorkersSalaries + allInternsSalaries
+
+    println(allSalaries / allPeople)
 }
