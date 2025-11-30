9e712607c80c5f1003914a6cfd5401e1533bc88d
Alena <luckystar@yandex-team.ru>
2025-11-30 20:18:48 +0300

    KS-2-3 train arrival 2

--- src/main/kotlin/Lesson_2/KS-2-3.kt
+++ src/main/kotlin/Lesson_2/KS-2-3.kt
@@ -5,13 +5,14 @@ fun main() {
 val departureHour = 9
 val departureMin = 39
 val durationMin = 457
+val minutesInHour = 60
 
-val durationHourMin = 457 / 60
-val totalDepartureMin = departureHour * 60 + departureMin
+val durationHourMin = durationMin / minutesInHour
+val totalDepartureMin = departureHour * minutesInHour + departureMin
 val totalArrivalMin = totalDepartureMin + durationMin
-val arrivalHour = totalArrivalMin / 60
+val arrivalHour = totalArrivalMin / minutesInHour
 
-val arrivalMin = totalArrivalMin % 60
+val arrivalMin = totalArrivalMin % minutesInHour
 
     println("$arrivalHour:$arrivalMin")
 }
