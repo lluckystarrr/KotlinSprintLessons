8798ffd700dac37b754de3fd68c1bccc6474e2c2
Alena <luckystar@yandex-team.ru>
2025-11-30 23:45:49 +0300

    KS-4-3 define conditions 2

--- src/main/kotlin/lesson_4/KS-4-3.kt
+++ src/main/kotlin/lesson_4/KS-4-3.kt
@@ -2,15 +2,16 @@ package org.example.lesson_4
 
 val SUNNY_WEATHER = true
 val OPEN_TENT = true
-val HUNIDITY = 20
-val SEASON = "!зима"
+val HUMIDITY = 20
+val WRONG_SEASON = "зима"
 
 fun main() {
 
     val sunnyToday = true
     val openTentToday = true
-    val hunidityToday = 20
+    val humidityToday = 20
     val seasonToday = "зима"
 
-    println(SUNNY_WEATHER == sunnyToday && OPEN_TENT == openTentToday && HUNIDITY == hunidityToday && SEASON == seasonToday)
+    println("Благоприятные ли условия сейчас для роста бобовых? " +
+            "${(SUNNY_WEATHER == sunnyToday) && (OPEN_TENT == openTentToday) && (HUMIDITY == humidityToday) && (WRONG_SEASON !== seasonToday)}")
 }
