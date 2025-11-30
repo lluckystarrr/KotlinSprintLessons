7b852b843d591fd2e40f7c38f6444dc38ea4bb66
Alena <luckystar@yandex-team.ru>
2025-11-30 20:36:22 +0300

    KS-3-3 recount multiplication table

--- src/main/kotlin/lesson_3/KS-3-3.kt
+++ src/main/kotlin/lesson_3/KS-3-3.kt
@@ -3,13 +3,15 @@ package org.example.lesson_3
 fun main() {
 var variable = 7
 
-   println("$variable x 1 = ${variable * 1}\n" +
-           "$variable x 2 = ${variable * 2}\n" +
-           "$variable x 3 = ${variable * 3}\n" +
-           "$variable x 4 = ${variable * 4}\n" +
-           "$variable x 5 = ${variable * 5}\n" +
-           "$variable x 6 = ${variable * 6}\n" +
-           "$variable x 7 = ${variable * 7}\n" +
-           "$variable x 8 = ${variable * 8}\n" +
-           "$variable x 9 = ${variable * 9}\n")
+println("""
+$variable x 1 = ${variable * 1}
+$variable x 2 = ${variable * 2}
+$variable x 3 = ${variable * 3}
+$variable x 4 = ${variable * 4}
+$variable x 5 = ${variable * 5}
+$variable x 6 = ${variable * 6}
+$variable x 7 = ${variable * 7}
+$variable x 8 = ${variable * 8}
+$variable x 9 = ${variable * 9}
+""")
 }
