5a07fbf823db124396d8936bda5c9063d2b3e9b5
Alena <luckystar@yandex-team.ru>
2025-12-15 17:15:36 +0300

    KS-6-4 guess number 2

--- src/main/kotlin/lesson_6/KS-6-4.kt
+++ src/main/kotlin/lesson_6/KS-6-4.kt
@@ -1,21 +1,25 @@
-package org.example.lesson_6
-
-const val WIN_NUMBER = 3
+//package org.example.lesson_6
+//
 const val MIN_NUMBER = 1
 const val MAX_NUMBER = 9
 
 fun main() {
+    val randomNumber = (1..9).random()
     println("Угадай число от $MIN_NUMBER до $MAX_NUMBER")
 
-    var number = readln().toInt()
+    var number: Int
     var attempt = 5
 
-    while ((attempt > 1) && (number != WIN_NUMBER)) {
-        println("Неверно. Осталось попыток - ${--attempt}")
+    do {
         number = readln().toInt()
-    }
-    if (number != WIN_NUMBER) {
-        println("Увы, было загадано число $WIN_NUMBER")
+        --attempt
+        if ((attempt > 0) && (number != randomNumber)) {
+            println("Неверно. Осталось попыток - $attempt")
+        }
+    } while ((attempt > 0) && (number != randomNumber))
+
+    if (number != randomNumber) {
+        println("Увы, было загадано число $randomNumber")
     } else {
         println("Это была великолепная игра!")
     }
