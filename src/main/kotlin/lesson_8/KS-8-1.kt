3672ac9b16b90cb5017fb6d5bc10dd51f086e5eb
Alena <luckystar@yandex-team.ru>
2025-12-20 20:20:56 +0300

    KS-8-1 count ad

---
+++ src/main/kotlin/lesson_8/KS-8-1.kt
@@ -0,0 +1,7 @@
+package org.example.lesson_8
+
+fun main() {
+
+    val weeksView = arrayOf(7, 8, 3, 1, 9, 4, 5)
+    println("${weeksView[0] + weeksView[1] + weeksView[2] + weeksView[3] + weeksView[4] + weeksView[5] + weeksView[6]}")
+}
