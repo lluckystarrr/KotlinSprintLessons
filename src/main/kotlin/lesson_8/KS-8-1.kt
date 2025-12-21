d207566b951a3649377804baebc70447d0a09218
Alena <luckystar@yandex-team.ru>
2025-12-21 13:09:45 +0300

    KS-8-1 count ad 2

--- src/main/kotlin/lesson_8/KS-8-1.kt
+++ src/main/kotlin/lesson_8/KS-8-1.kt
@@ -3,5 +3,10 @@ package org.example.lesson_8
 fun main() {
 
     val weeksView = arrayOf(7, 8, 3, 1, 9, 4, 5)
-    println("${weeksView[0] + weeksView[1] + weeksView[2] + weeksView[3] + weeksView[4] + weeksView[5] + weeksView[6]}")
+    var sumView = 0
+
+    for (dayView in weeksView) {
+        sumView += dayView
+    }
+    print("$sumView")
 }
