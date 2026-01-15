408c04a323fca76ad4541cd64115cfddb267371f
Alena <luckystar@yandex-team.ru>
2026-01-15 17:33:41 +0300

    KS-11-1 user info

---
+++ src/main/kotlin/lesson_11/KS-11-1.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_11
+
+fun main() {
+    val user1 = Info(
+        id = 1,
+        login = "luckystar",
+        password = "1234",
+        mail = "luckystar@mail.ru"
+    )
+
+    val user2 = Info(
+        id = 2,
+        login = "blackberry",
+        password = "y4tv",
+        mail = "blackberry@yandex.ru"
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.mail)
+    println()
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.mail)
+}
