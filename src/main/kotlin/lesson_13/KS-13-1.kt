d7fd4edcff19963a6842e0ff9594363b5d24ad98
Alena <luckystar@yandex-team.ru>
2026-03-09 18:55:32 +0300

    KS-13-3 phone book with lists

--- src/main/kotlin/lesson_13/KS-13-1.kt
+++ src/main/kotlin/lesson_13/KS-13-1.kt
@@ -2,14 +2,7 @@ package org.example.lesson_13
 
 class Info (val name: String, val number: Long, val company: String?,)
 
-{
-    fun showData(){
-        println("Имя: $name, Номер: $number, Компания: $company")
-    }
-}
 fun main() {
     val contact1 = Info("Максим", 89164751213, null)
     val contact2 = Info("Алексей", 890835674378, "Березка")
-
-    contact1.showData()
 }
