c39d091ab09b853cf6c40e031674e56edfba09a5
Alena <luckystar@yandex-team.ru>
2026-03-09 18:50:07 +0300

    KS-13-3 phone book with lists

--- src/main/kotlin/lesson_13/KS-13-3.kt
+++ src/main/kotlin/lesson_13/KS-13-3.kt
@@ -10,7 +10,7 @@ class Contact(val name: String, val number: Long, val company: String? = null) {
 fun main() {
     val companyContact = listOf(
         Contact("Максим", 89164751213),
-        Contact("Алексей", 890835674378, "Березка"),
+        Contact("Андрей", 890835674378, "Березка"),
         Contact("Мария", 89163451234, "Сахарок"),
         Contact("Светална", 89178978932),
         Contact("Егор", 89165491234, "null")
