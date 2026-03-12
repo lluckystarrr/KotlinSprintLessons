7bbede7990c44e85600a17e33da3de80864d52f3
Alena <luckystar@yandex-team.ru>
2026-03-13 00:15:16 +0300

    KS-13-4 add contacts to phone book

---
+++ src/main/kotlin/lesson_13/KS-13-4.kt
@@ -0,0 +1,39 @@
+package org.example.lesson_13
+
+class Contacts(val name: String, val number: Long, val company: String? = null) {
+
+    fun showData() {
+        println("Имя: $name, Номер: $number, Компания: ${company ?: "<не указано>"}")
+    }
+}
+
+fun main() {
+    val phoneBook = mutableListOf<Contacts>()
+    do {
+        println("Введите имя")
+        val inputName = readln()
+
+        println("Введите номер")
+        val inputNumber = readln().toLongOrNull()
+        if (inputNumber == null) {
+            println("Вы не заполнили номер")
+            return
+        }
+
+        println("Введите компанию")
+        var inputCompany = readln()
+        if (inputCompany == "") {
+            inputCompany = null.toString()
+        }
+
+        val contact6 = Contacts(inputName, inputNumber, inputCompany)
+        phoneBook.add(contact6)
+
+        println("Добавить еще компанию?")
+        val addContact = readln()
+    } while (
+        addContact.equals("да", ignoreCase = true)
+    )
+
+    phoneBook.forEach { it.showData() }
+}
