1d8ac18a762b8f737ed8f2c56d4883691c460d04
Alena <luckystar@yandex-team.ru>
2026-03-31 14:54:04 +0300

    KS-14-2 ship description and methods 2

--- src/main/kotlin/lesson_14/KS-14-2.kt
+++ src/main/kotlin/lesson_14/KS-14-2.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_14
 
 open class Ship2(
-    val speed: Int = 10,
-    val carryingCapacity: Int = 10,
-    val capacity: Int = 10,
-    val breakIce: Boolean = false,
+    open val speed: Int = 10,
+    open val carryingCapacity: Int = 10,
+    open val capacity: Int = 10,
+    open val breakIce: Boolean = false,
 ) {
     fun printInfo() {
         println("Скорость: $speed")
@@ -14,31 +14,30 @@ open class Ship2(
     }
 }
 
-class Liner2(
-    speed: Int,
-    carryingCapacity: Int,
-    capacity: Int,
-) : Ship2(speed, carryingCapacity, capacity) {
+class Liner2 : Ship2() {
+    override val capacity: Int = 20
+
     fun extentHorizontalLadder() {
         println("Способ погрузки: Выдвигает горизонтальный трап со шкафута")
     }
 }
 
-class Cargo2(
-    speed: Int,
-    carryingCapacity: Int,
-    capacity: Int
-) : Ship2(speed, carryingCapacity, capacity) {
+class Cargo2 : Ship2() {
+    override val speed: Int = 5
+    override val carryingCapacity: Int = 15
+    override val capacity: Int = 5
+
     fun activateLoadingCrane() {
         println("Способ погрузки: Активирует погрузочный кран")
     }
 }
 
-class Icebreaker2(
-    speed: Int,
-    carryingCapacity: Int,
-    capacity: Int,
-) : Ship2(speed, carryingCapacity, capacity, breakIce = true) {
+class Icebreaker2 : Ship2() {
+    override val speed: Int = 6
+    override val carryingCapacity: Int = 8
+    override val capacity: Int = 5
+    override val breakIce: Boolean = true
+
     fun openGate() {
         println("Способ погрузки: Открывает ворота со стороны кормы")
     }
@@ -46,17 +45,17 @@ class Icebreaker2(
 
 fun main() {
 
-    val liner2 = Liner2(10, 10, 10)
+    val liner2 = Liner2()
     println("Лайнер:")
     liner2.printInfo()
     liner2.extentHorizontalLadder()
 
-    val cargo2 = Cargo2(5, 15, 5)
+    val cargo2 = Cargo2()
     println("\nГрузовой корабль:")
     cargo2.printInfo()
     cargo2.activateLoadingCrane()
 
-    val iceBreaker2 = Icebreaker2(6, 8, 5)
+    val iceBreaker2 = Icebreaker2()
     println("\nЛедокол:")
     iceBreaker2.printInfo()
     iceBreaker2.openGate()
