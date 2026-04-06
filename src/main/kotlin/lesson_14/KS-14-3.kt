ff8eac613a9e28e1581e782644e8b4a8413633b4
Alena <luckystar@yandex-team.ru>
2026-04-06 15:49:35 +0300

    KS-14-3 shapes 2

--- src/main/kotlin/lesson_14/KS-14-3.kt
+++ src/main/kotlin/lesson_14/KS-14-3.kt
@@ -1,6 +1,8 @@
 package org.example.lesson_14
+import kotlin.math.PI
 
-const val PI = 3.14
+const val COLOR_BLACK = "черный"
+const val COLOR_WHITE = "белый"
 
 abstract class Figure(val color: String) {
     abstract fun areaCalculate(): Double
@@ -36,21 +38,19 @@ class Rectangle(
 }
 
 fun main() {
-    val krug1 = Circle("черный", 12)
-    val krug2 = Circle("белый", 11)
-    val krug3 = Circle("белый", 10)
-    val krug4 = Circle("черный", 9)
-    val pryamougolnik1 = Rectangle("черный", 3, 3)
-    val pryamougolnik2 = Rectangle("черный", 4, 4)
-    val pryamougolnik3 = Rectangle("белый", 5, 5)
-    val pryamougolnik4 = Rectangle("белый", 5, 5)
+    val krug1 = Circle(COLOR_BLACK, 12)
+    val krug2 = Circle(COLOR_WHITE, 11)
+    val krug3 = Circle(COLOR_WHITE, 10)
+    val krug4 = Circle(COLOR_BLACK, 9)
+    val pryamougolnik1 = Rectangle(COLOR_BLACK, 3, 3)
+    val pryamougolnik2 = Rectangle(COLOR_BLACK, 4, 4)
+    val pryamougolnik3 = Rectangle(COLOR_WHITE, 5, 5)
+    val pryamougolnik4 = Rectangle(COLOR_WHITE, 5, 5)
 
-    val list = mutableListOf(krug1, krug2, krug3, krug4, pryamougolnik1, pryamougolnik2, pryamougolnik3, pryamougolnik4)
+    val list = listOf(krug1, krug2, krug3, krug4, pryamougolnik1, pryamougolnik2, pryamougolnik3, pryamougolnik4)
 
-
-    val blacklist = list.filter { it.color == "черный" }.sumOf { it.perimeterCalculate() }
-    val whitelist = list.filter { it.color == "белый" }.sumOf { it.areaCalculate() }
-
+    val blacklist = list.filter { it.color == COLOR_BLACK }.sumOf { it.perimeterCalculate() }
+    val whitelist = list.filter { it.color == COLOR_WHITE }.sumOf { it.areaCalculate() }
 
     println("сумма периметров черного $blacklist")
     println("сумма площади белого $whitelist")
