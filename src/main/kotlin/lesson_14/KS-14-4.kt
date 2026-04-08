eb2de28374dae2e7c2cf4c48c7ff175ecd48e1cf
Alena <luckystar@yandex-team.ru>
2026-04-08 17:47:20 +0300

    KS-14-4 planets and satellites 2

--- src/main/kotlin/lesson_14/KS-14-4.kt
+++ src/main/kotlin/lesson_14/KS-14-4.kt
@@ -1,32 +1,32 @@
 package org.example.lesson_14
 
-open class astronomicalObject(
+open class AstronomicalObject(
     val name: String,
     val atmosphere: Boolean,
     val suitable: Boolean,
-) {}
+)
 
-class planet(
+class Planet(
     name: String,
     atmosphere: Boolean,
     suitable: Boolean,
-    val haveSatellites: List<satellite>?,
-) : astronomicalObject(name, atmosphere, suitable)
+    val haveSatellites: List<Satellite>,
+) : AstronomicalObject(name, atmosphere, suitable)
 
-class satellite(
+class Satellite(
     name: String,
     atmosphere: Boolean,
     suitable: Boolean,
-) : astronomicalObject(name, atmosphere, suitable)
+) : AstronomicalObject(name, atmosphere, suitable)
 
 fun main() {
-    val satellite1 = satellite("спутник1", false, false)
-    val satellite2 = satellite("спутник2", false, false)
+    val satellite1 = Satellite("спутник1", false, false)
+    val satellite2 = Satellite("спутник2", false, false)
 
-    val planet1 = planet("Сатурн", false, false, listOf(satellite1, satellite2))
+    val planet1 = Planet("Сатурн", false, false, listOf(satellite1, satellite2))
 
     println("планета: ${planet1.name}")
     println("атмосфера: ${planet1.atmosphere}")
     println("пригодна: ${planet1.suitable},")
-    println("спутники: ${planet1.haveSatellites?.joinToString { it.name }}")
+    println("спутники: ${planet1.haveSatellites.joinToString { it.name }}")
 }
