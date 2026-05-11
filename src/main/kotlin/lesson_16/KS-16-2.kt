f89636e68dc3797508af374fe22423eabb4312f0
Alena <luckystar@yandex-team.ru>
2026-05-11 12:19:43 +0300

    KS-16-2 circumference and circle area

--- src/main/kotlin/lesson_16/KS-16-2.kt
+++ src/main/kotlin/lesson_16/KS-16-2.kt
@@ -1,20 +1,24 @@
 package org.example.lesson_16
 
+import kotlin.math.pow
+
+private const val PI = 3.14
+
 class Circle12(private val radius: Int) {
-    private val PI = 3.14
 
     fun countCircumference(): Double {
         return 2 * PI * radius
     }
 
     fun countCircleArea(): Double {
-        return PI * radius * radius
+        return PI * radius.toDouble().pow(2)
     }
 }
-    fun main() {
-        val circle1 = Circle12(radius = 12)
-        val circumference = circle1.countCircumference()
-        val circleArea = circle1.countCircleArea()
-        println(circumference)
-        println(circleArea)
-    }
+
+fun main() {
+    val circle1 = Circle12(radius = 12)
+    val circumference = circle1.countCircumference()
+    val circleArea = circle1.countCircleArea()
+    println(circumference)
+    println(circleArea)
+}
