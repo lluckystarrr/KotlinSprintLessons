ab0cf9cc080ea2a1b3480bb2e19317017cc20be8
Alena <luckystar@yandex-team.ru>
2026-08-19 17:19:23 +0300

    KS-19-3 spaceship game 3

--- src/main/kotlin/lesson_19/KS-19-3.kt
+++ src/main/kotlin/lesson_19/KS-19-3.kt
@@ -3,12 +3,10 @@ package org.example.lesson_19
 class Spaceship {
     fun takeOff() {
         // TODO: добавить логику взлета
-        println("Корабль взлетает")
     }
 
     fun land() {
         // TODO: добавить логику посадки
-        println("Корабль приземляется")
     }
 
     fun shootAsteroid() {
