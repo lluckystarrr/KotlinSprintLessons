829b57d6a43547ec487de98debfc0c04c063f084
Alena <luckystar@yandex-team.ru>
2026-08-19 17:15:02 +0300

    KS-19-4 tank game

---
+++ src/main/kotlin/lesson_19/KS-19-4.kt
@@ -0,0 +1,39 @@
+package org.example.lesson_19
+
+enum class Ammo(val damage: Int) {
+    BLUE(5),
+    GREEN(10),
+    RED(20)
+}
+
+class Tank {
+    private var currentAmmo: Ammo? = null
+
+    fun loadAmmo(ammo: Ammo) {
+        currentAmmo = ammo
+        println("Танк заряжен патронами: ${ammo.name} (урон: ${ammo.damage})")
+    }
+
+    fun fire() {
+        if (currentAmmo != null) {
+            println("Нанесен урон: ${currentAmmo!!.damage} единиц")
+        } else {
+            println("Танк не заряжен")
+        }
+    }
+}
+
+fun main() {
+    val tank = Tank()
+
+    println("Первый выстрел")
+    tank.fire()
+
+    tank.loadAmmo(Ammo.BLUE)
+    println("Второй выстрел. Стреляем синими")
+    tank.fire()
+
+    tank.loadAmmo(Ammo.GREEN)
+    println("Третий выстрел. Стреляем зелеными")
+    tank.fire()
+}
