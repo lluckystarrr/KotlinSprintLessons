d151d82b1db12f2bf563b42fdbb32f4496334715
Alena <luckystar@yandex-team.ru>
2026-08-27 14:22:01 +0300

    KS-22-1 regular and data books 2

--- src/main/kotlin/lesson_22/KS-22-1.kt
+++ src/main/kotlin/lesson_22/KS-22-1.kt
@@ -1,16 +1,21 @@
 package org.example.lesson_22
 
-class RegularBook(val title: String, val author: String,)
-data class DataBook(val title: String, val author: String,)
+class RegularBook(val title: String, val author: String)
+data class DataBook(val title: String, val author: String)
 
 fun main() {
     val regular1 = RegularBook("Пес и кошка", "Ринатова")
     val regular2 = RegularBook("Пес и кошка", "Ринатова")
 
     val data1 = DataBook("Пес и кошка", "Ринатова")
-    val data2 = DataBook("Пес и кошка", "ТоРинатовалстой")
+    val data2 = DataBook("Пес и кошка", "Ринатова")
 
-    // Сравниваем
     println("Обычный класс: ${regular1 == regular2}")
     println("Data class: ${data1 == data2}")
 }
+
+//RegularBook -> false
+//- Обычный класс сравнивает сами объекты (разные объекты, получаем false)
+//
+//DataBook -> true
+//- Data class сравнивает данные внутри объектов (данные одинаковые, получаем true)
